03c999a6eb4092005f05f91044ec1b1aa7455741
Jennifer Man <jenniferman702@gmail.com>
2022-10-30 12:03:51 -0400

    commit tree 2753846e04b5cbd324f2273b99b38541ca6bea44

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,18 @@
 import React from "react";
+import Home from './Home.js'
+import About from "./About.js"
+
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return (
+    <div>
+      <nav>NavBar
+        <a href="#home">{Home}</a>
+        <a href='#about'>{About}</a>
+      </nav>
+    </div>
+  )
 }
 
 export default NavBar;
